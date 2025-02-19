e569554545c802231deb3512c56caf3d89565fe6
Mustafa Olmezses <mustafaolmezses@gmail.com>
2025-02-19 21:45:27 +0300

    Initial Commit

---
+++ CoreDataTodo/CoreDataTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTodoApp.swift
+//  CoreDataTodo
+//
+//  Created by Mustafa Ölmezses on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
